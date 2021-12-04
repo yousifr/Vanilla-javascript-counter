464c9d3608f513eedec12bf1282f9b351324dbf9
yousifr <yousifr.ismail@gmail.com>
2021-12-04 09:12:29 +0300

    first deploy

---
+++ script.js
@@ -0,0 +1,17 @@
+const value = document.getElementById("value");
+const btns = document.querySelectorAll(".btn");
+
+let count = 0;
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    value.textContent = count;
+  });
+});
